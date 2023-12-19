f9b760cf60978ee99d785bf009bf1c861cdfedcf
Odame <kwesi.danquah@daaseng.com>
2023-12-19 17:19:29 +0000

    Updated home page template and error handling in the home() function

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 const METHOD_NOT_ALLOWED = 405
-
-// define a home handler function which writes a byte slice containing
-// "Hello from snippetbox" as the response body.
+const INTERNAL_SERVER_ERROR = 500
 
 func home(w http.ResponseWriter, r *http.Request) {
 
@@ -19,7 +19,33 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 
 	}
-	w.Write([]byte("Hello from snippetbox"))
+
+	// Use the template.ParseFiles() function to read the template file into a
+	// template set. If there's an error, we log the detailed error message and
+	// use the http.Error() function to send a generic 500 Internal Server Error
+	// response to the user.
+
+	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+
+	if err != nil {
+		log.Println(err.Error())
+
+		http.Error(w, "Internal Server Error", INTERNAL_SERVER_ERROR)
+
+		return
+	}
+
+	// We then use the Execute() method on the template set to write the template
+	// content as the response body. The last parameter to Execute() represents
+	// dynamic data that we want tp pass in, which for now we'll leave as nil.
+
+	err = ts.Execute(w, nil)
+
+	if err != nil {
+		log.Println(err.Error())
+
+		http.Error(w, "Internal Server Error", INTERNAL_SERVER_ERROR)
+	}
 }
 
 func snippet(w http.ResponseWriter, r *http.Request) {
