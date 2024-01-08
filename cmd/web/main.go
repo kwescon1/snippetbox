c94928c2972ddff4785c4e09b4cbda7177c4392b
Odame <kwesi.danquah@daaseng.com>
2024-01-08 20:16:28 +0000

    configure mysql database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"os"
@@ -21,6 +23,9 @@ func main() {
 	// flag will be stored in the addr variable at runtime.
 
 	addr := flag.String("addr", "4000", "HTTP network address")
+
+	// Define a new command line flag for the mysql DNS string
+	dsn := flag.String("dsn", "my_user:my_password@tcp(localhost:3308)/snippetbox?parseTime=true", "MySQL database connection string")
 
 	// Importantly, we use the flag.Parse() function to parse the command-line
 	// This reads in the command-line flag value and assigns it to the addr
@@ -42,6 +47,16 @@ func main() {
 	// file name and line number
 
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	// We also defer a call to db.Close(), so that the connection pool is close
+	// before the main() function exits.
+	defer db.Close()
 
 	// Initialize a new instance of application containing the dependencies
 
@@ -69,6 +84,23 @@ func main() {
 	infoLog.Printf("Starting server on %s", *addr)
 
 	//Call the listenAndServe() method on our new http.Server struct.
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+// The openDB() function wraps a sql.Open()and returns a sql.DB connection pool
+// for a given DSN
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
